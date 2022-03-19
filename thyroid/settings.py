b1fd423da25b67dfb16aff7a6d9093091d91987a
SabarinathK <sabarinathodc@gmail.com>
2022-03-19 13:08:35 +0530

    'updated-settings'

--- thyroid/settings.py
+++ thyroid/settings.py
@@ -122,7 +122,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [
+     os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
