3d04aa9da5c7c0e6c0146550f4a24a5f97cc20fa
SabarinathK <sabarinathodc@gmail.com>
2022-03-19 20:50:45 +0530

    settings-is-updated'

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,13 @@ from . import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', views.home,name='home'),
     path('predict/',views.prediction,name='prediction')
     
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#thyroid\static\images\tyroid.jpg
--- thyroid/settings.py
+++ thyroid/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,13 +122,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'static'),
-        ]
+STATIC_URL = 'static/'
+MEDIA_URL ='/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+        
 # Extra places for collectstatic to find static files.
 
 
