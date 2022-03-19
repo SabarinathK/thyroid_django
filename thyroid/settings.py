c6ba5408ef2d3754d428099cfe533f69c557d2ee
SabarinathK <sabarinathodc@gmail.com>
2022-03-19 12:05:50 +0530

    'updated-settings'

--- thyroid/settings.py
+++ thyroid/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)!_b6+a77x2-irr#@3)6pj8rm3*ti!$l_43*%eu-bs&%17-5zq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://thyroid-django.herokuapp.com/']
+ALLOWED_HOSTS = ['thyroid-django.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -118,14 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
