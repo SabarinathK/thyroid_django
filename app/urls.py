45ea4091ae20fb057903b168beb2c0b3ad6d245e
SabarinathK <sabarinathodc@gmail.com>
2022-03-19 12:48:12 +0530

    'updated-settings'

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home,name='home'),
     path('predict/',views.prediction,name='prediction')
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- thyroid/settings.py
+++ thyroid/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['https://thyroid-django.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'thyroid.urls'
 
@@ -119,11 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
